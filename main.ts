5680bda114a71b796bfbc03be83ec85eccfc6356
Eduardo Hernández Valdez <lalohdz.404@gmail.com>
2020-09-17 13:26:18 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Ec=")
+    basic.showNumber(masa * velocidad ** 2 / 2)
+})
+let velocidad = 0
+let masa = 0
+basic.showString("Eduardo Hernández Valdez")
+basic.showString("A01285233")
+masa = 17.78
+velocidad = 15
